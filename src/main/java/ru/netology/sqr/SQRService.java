81e8867a5f66e71f70fbfb54dc891cf27eac0b01
ALEXGOT7 <103954512+ALEXGOT7@users.noreply.github.com>
2022-10-14 13:34:28 +0300

    Update SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,7 +1,7 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int range(int a, int b) {
+    public int range(int min, int max) {
         int amount = 0;
         for (int i = 10; i < 100; i++) {
             if ((i * i >= a) & (i * i <= b)) {
